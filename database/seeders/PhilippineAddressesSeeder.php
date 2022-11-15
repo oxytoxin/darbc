eb133b13422d41dcdae61611b9e4c70e22d03fb4
oxytoxin <oxytoxinsgrace@gmail.com>
2022-11-15 12:52:23 +0800

    wip

---
+++ database/seeders/PhilippineAddressesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PhilippineAddressesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Seeding regions!');
+        DB::unprepared(file_get_contents(database_path('ph_addresses/regions.sql')));
+        $this->command->info('Regions seeded!');
+        $this->command->info('Seeding provinces!');
+        DB::unprepared(file_get_contents(database_path('ph_addresses/provinces.sql')));
+        $this->command->info('Provinces seeded!');
+        $this->command->info('Seeding cities!');
+        DB::unprepared(file_get_contents(database_path('ph_addresses/cities.sql')));
+        $this->command->info('Cities seeded!');
+        $this->command->info('Seeding barangays!');
+        DB::unprepared(file_get_contents(database_path('ph_addresses/barangays.sql')));
+        $this->command->info('Barangays seeded!');
+    }
+}
