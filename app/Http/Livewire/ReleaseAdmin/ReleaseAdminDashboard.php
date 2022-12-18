03695d3574afb554ebe21516b1183dc71cd8efeb
oxytoxin <oxytoxinsgrace@gmail.com>
2022-12-18 15:05:28 +0800

    admin filters

--- app/Http/Livewire/ReleaseAdmin/ReleaseAdminDashboard.php
+++ app/Http/Livewire/ReleaseAdmin/ReleaseAdminDashboard.php
@@ -13,21 +13,23 @@ use Carbon\Carbon;
 class ReleaseAdminDashboard extends Component
 {
     public $release_id;
-    public $since;
+    public $from;
+    public $to;
 
     public function mount()
     {
-        $this->since = today()->format('Y-m-d');
+        $this->from = today()->format('Y-m-d H:i');
+        $this->to = today()->format('Y-m-d H:i');
         $this->release_id = Release::latest()->whereDisbursed(true)->first()->id;
     }
 
     public function render()
     {
-        $latest_release = Release::whereDisbursed(true)->latest()
+        $selected_release = Release::whereDisbursed(true)->latest()
             ->withCount(['dividends', 'released_dividends'])
             ->withSum('released_dividends as released_dividends_gross', 'gross_amount')
             ->withSum('released_dividends as released_dividends_deductions', 'deductions_amount')
-            ->first();
+            ->find($this->release_id);
 
 
         return view('livewire.release-admin.release-admin-dashboard', [
@@ -36,13 +38,22 @@ class ReleaseAdminDashboard extends Component
             'original_members_count' => MemberInformation::original()->count(),
             'replacement_members_count' => MemberInformation::replacement()->count(),
             'members_on_hold_count' => User::onHold()->count(),
-            'latest_release' => $latest_release,
+            'selected_release' => $selected_release,
             'releases' => Release::get(),
             'recent_transactions' => Dividend::whereStatus(Dividend::RELEASED)->with(['cashier', 'user', 'release'])->latest('released_at')->take(7)->get(),
             'cashiers' => User::whereRelation('roles', 'role_id', Role::CASHIER)
-                ->withCount(['cashier_released_dividends' => fn ($query) => $query->whereDate('released_at', $this->since)->whereReleaseId($this->release_id)])
-                ->withSum(['cashier_released_dividends as cashier_released_dividends_gross' => fn ($query) => $query->whereDate('released_at', $this->since)->whereReleaseId($this->release_id)], 'gross_amount')
-                ->withSum(['cashier_released_dividends as cashier_released_dividends_deductions' => fn ($query) => $query->whereDate('released_at', $this->since)->whereReleaseId($this->release_id)], 'deductions_amount')
+                ->withCount(['cashier_released_dividends' => fn ($query) => $query
+                    ->when($this->from, fn ($q) => $q->where('released_at', '>=', $this->from))
+                    ->when($this->to, fn ($q) => $q->where('released_at', '<=', $this->to))
+                    ->whereReleaseId($this->release_id)])
+                ->withSum(['cashier_released_dividends as cashier_released_dividends_gross' => fn ($query) => $query
+                    ->when($this->from, fn ($q) => $q->where('released_at', '>=', $this->from))
+                    ->when($this->to, fn ($q) => $q->where('released_at', '<=', $this->to))
+                    ->whereReleaseId($this->release_id)], 'gross_amount')
+                ->withSum(['cashier_released_dividends as cashier_released_dividends_deductions' => fn ($query) => $query
+                    ->when($this->from, fn ($q) => $q->where('released_at', '>=', $this->from))
+                    ->when($this->to, fn ($q) => $q->where('released_at', '<=', $this->to))
+                    ->whereReleaseId($this->release_id)], 'deductions_amount')
                 ->get(),
         ]);
     }
