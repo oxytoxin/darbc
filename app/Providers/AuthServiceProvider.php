9aec8644bd61c363fdd3880965f8b9510b345105
oxytoxin <oxytoxinsgrace@gmail.com>
2022-12-18 12:48:43 +0800

    allow web tinker using superadmin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebTinker', function ($user = null) {
+            return $user->username = 'admin';
+        });
     }
 }
