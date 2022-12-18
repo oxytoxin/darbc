53a83ccaa03ac4322abf1182e22ec6708660e566
oxytoxin <oxytoxinsgrace@gmail.com>
2022-12-18 13:50:23 +0800

    member dividends urls in lineage

--- resources/views/livewire/shared/member-dividends.blade.php
+++ resources/views/livewire/shared/member-dividends.blade.php
@@ -3,9 +3,7 @@
         <div>
             <h4 class="text-lg font-semibold">{{ $member->user->full_name }}</h4>
             <h5 class="text-sm font-semibold text-gray-600">
-                {{ $member->succession_number == '0' ?
-                'Original Owner' :
-                ordinal($member->succession_number) . ' Successor' }}
+                {{ $member->succession_number == '0' ? 'Original Owner' : ordinal($member->succession_number) . ' Successor' }}
             </h5>
         </div>
         <div>
@@ -19,13 +17,13 @@
         <div>
             <p class="font-semibold text-gray-600">Release Status</p>
             @if ($member->user->active_restriction)
-            <div>
-                <span class="bg-red-500/[8%] py-1 px-4 rounded-full text-red-700">on-hold</span>
-            </div>
+                <div>
+                    <span class="bg-red-500/[8%] py-1 px-4 rounded-full text-red-700">on-hold</span>
+                </div>
             @else
-            <div>
-                <span class="bg-custom-green/[8%] py-1 px-4 rounded-full text-custom-green">release</span>
-            </div>
+                <div>
+                    <span class="bg-custom-green/[8%] py-1 px-4 rounded-full text-custom-green">release</span>
+                </div>
             @endif
         </div>
     </div>
@@ -34,28 +32,22 @@
             <p class="font-semibold">Account Ownership</p>
             <div>
                 @foreach ($lineage_members as $lineage_member)
-                <div class="flex items-center gap-2 @if (!$loop->first)
-                    px-8
-                @endif">
-                    @if (!$loop->first)
-                    <svg width="16" height="16" viewBox="0 0 5 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M2.0702 3.72808L0.0385742 1.78091L1.05403 0.806641L4.1011 3.72808L1.05403 6.64952L0.0385742 5.67525L2.0702 3.72808Z"
-                            fill="#EF9A47" />
-                    </svg>
-                    @endif
-                    <div>
-                        <p>{{ $lineage_member->user->full_name }}</p>
-                        <h5 class="text-xs font-semibold text-gray-400">
-                            {{ $lineage_member->succession_number == '0' ?
-                            'Original Owner' :
-                            ordinal($lineage_member->succession_number) . ' Successor' }}
-                            @if ($lineage_member->id == $member->id)
-                            <span class="text-green-600">(Current)</span>
-                            @endif
-                        </h5>
+                    <div class="flex items-center gap-2 @if (!$loop->first) px-8 @endif">
+                        @if (!$loop->first)
+                            <svg width="16" height="16" viewBox="0 0 5 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.0702 3.72808L0.0385742 1.78091L1.05403 0.806641L4.1011 3.72808L1.05403 6.64952L0.0385742 5.67525L2.0702 3.72808Z" fill="#EF9A47" />
+                            </svg>
+                        @endif
+                        <div>
+                            <a href="{{ route(request()->route()->getName(),['member' => $lineage_member]) }}">{{ $lineage_member->user->full_name }}</a>
+                            <h5 class="text-xs font-semibold text-gray-400">
+                                {{ $lineage_member->succession_number == '0' ? 'Original Owner' : ordinal($lineage_member->succession_number) . ' Successor' }}
+                                @if ($lineage_member->id == $member->id)
+                                    <span class="text-green-600">(Current)</span>
+                                @endif
+                            </h5>
+                        </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </div>
@@ -71,49 +63,47 @@
                     </tr>
                 <tbody>
                     @php
-                    $isCashier = auth()->user()->roles()->where('name', 'cashier')->exists();
+                        $isCashier = auth()
+                            ->user()
+                            ->roles()
+                            ->where('name', 'cashier')
+                            ->exists();
                     @endphp
                     @forelse ($dividends_by_year as $year => $dividends)
-                    @foreach ($dividends as $dividend)
-                    <tr>
-                        @if ($loop->first)
-                        <td rowspan="{{ count($dividends) }}" class="@if ($loop->parent->odd)
-                            bg-[#E3E3E3]
+                        @foreach ($dividends as $dividend)
+                            <tr>
+                                @if ($loop->first)
+                                    <td rowspan="{{ count($dividends) }}" class="@if ($loop->parent->odd) bg-[#E3E3E3]
                             @else
-                            bg-[#EF9A47]
-                        @endif py-4">
-                            <p class="text-center -rotate-90">{{ $year }}</p>
-                        </td>
-                        @endif
-                        <td class="px-2 py-2 border-b">{{ $dividend->release->name }}</td>
-                        <td class="px-2 py-2 border-b">{{ Akaunting\Money\Money::PHP($dividend->net_amount, true) }}
-                        </td>
-                        <td class="px-2 py-2 border-b">{{ $dividend->release->created_at->format('F d, Y') }}</td>
-                        <td class="px-2 py-2 border-b">
-                            <a href="{{ $dividend->status == App\Models\Dividend::FOR_RELEASE && $isCashier
-                            ? route('cashier.dividends.manage', ['dividend' => $dividend])
-                            : " #" }}" class="px-2 py-1 text-xs font-semibold rounded-full {{
-                                     match($dividend->status) {
-                                    App\Models\Dividend::PENDING => 'bg-yellow-200 text-yellow-800',
-                                    App\Models\Dividend::FOR_RELEASE => 'bg-success-200 text-success-800 underline',
-                                    App\Models\Dividend::ON_HOLD => 'bg-red-200 text-red-800',
-                                    App\Models\Dividend::RELEASED => 'bg-gray-200 text-gray-800'
-                                }
-                                }}">{{
-                                match($dividend->status){
-                                App\Models\Dividend::PENDING => 'Pending',
-                                App\Models\Dividend::FOR_RELEASE => 'For Release',
-                                App\Models\Dividend::ON_HOLD => 'On Hold',
-                                App\Models\Dividend::RELEASED => 'Released',
-                                } }}
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
+                            bg-[#EF9A47] @endif py-4">
+                                        <p class="text-center -rotate-90">{{ $year }}</p>
+                                    </td>
+                                @endif
+                                <td class="px-2 py-2 border-b">{{ $dividend->release->name }}</td>
+                                <td class="px-2 py-2 border-b">{{ Akaunting\Money\Money::PHP($dividend->net_amount, true) }}
+                                </td>
+                                <td class="px-2 py-2 border-b">{{ $dividend->release->created_at->format('F d, Y') }}</td>
+                                <td class="px-2 py-2 border-b">
+                                    <a href="{{ $dividend->status == App\Models\Dividend::FOR_RELEASE && $isCashier ? route('cashier.dividends.manage', ['dividend' => $dividend]) : ' #' }}"
+                                        class="px-2 py-1 text-xs font-semibold rounded-full {{ match ($dividend->status) {
+                                            App\Models\Dividend::PENDING => 'bg-yellow-200 text-yellow-800',
+                                            App\Models\Dividend::FOR_RELEASE => 'bg-success-200 text-success-800 underline',
+                                            App\Models\Dividend::ON_HOLD => 'bg-red-200 text-red-800',
+                                            App\Models\Dividend::RELEASED => 'bg-gray-200 text-gray-800',
+                                        } }}">{{ match ($dividend->status) {
+                                            App\Models\Dividend::PENDING => 'Pending',
+                                            App\Models\Dividend::FOR_RELEASE => 'For Release',
+                                            App\Models\Dividend::ON_HOLD => 'On Hold',
+                                            App\Models\Dividend::RELEASED => 'Released',
+                                        } }}
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
                     @empty
-                    <tr>
-                        <td colspan="5" class="py-4 text-sm text-center">No dividends found for this user.</td>
-                    </tr>
+                        <tr>
+                            <td colspan="5" class="py-4 text-sm text-center">No dividends found for this user.</td>
+                        </tr>
                     @endforelse
                 </tbody>
                 </thead>
