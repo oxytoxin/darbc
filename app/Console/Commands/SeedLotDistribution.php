3faa5df1bb86948312dc1be29951e0e4c3f78882
oxytoxin <oxytoxinsgrace@gmail.com>
2023-02-08 20:50:59 +0800

    lot seeder

--- app/Console/Commands/SeedLotDistribution.php
+++ app/Console/Commands/SeedLotDistribution.php
@@ -36,6 +36,32 @@ class SeedLotDistribution extends Command
         Schema::dropIfExists('block_addresses');
         Schema::dropIfExists('area_addresses');
         Schema::dropIfExists('lot_addresses');
+        $model_files = [
+            app_path('Models/AreaAddress.php'),
+            app_path('Models/BlockAddress.php'),
+            app_path('Models/LandDocuments.php'),
+            app_path('Models/LotAdress.php'),
+            app_path('Models/LotInformation.php'),
+            app_path('Models/MapImage.php'),
+        ];
+        $migration_files = [
+            'migrations/2022_01_12_124342_create_block_addresses_table.php',
+            'migrations/2023_01_12_124344_create_lot_addresses_table.php',
+            'migrations/2023_01_12_124345_create_area_addresses_table.php',
+            'migrations/2023_01_12_154342_create_lot_information_table.php',
+            'migrations/2023_01_16_103631_create_map_images_table.php',
+            'migrations/2023_01_16_145736_create_land_documents_table.php',
+        ];
+        foreach ($model_files as $key => $file) {
+            if (file_exists($file)) {
+                unlink($file);
+            }
+        }
+        foreach ($migration_files as $key => $file) {
+            if (file_exists($file)) {
+                unlink($file);
+            }
+        }
         return Command::SUCCESS;
     }
 }
