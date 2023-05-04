68f3e439ad596b600096e050f1edf69bc9439ac1
oxytoxin <oxytoxinsgrace@gmail.com>
2023-05-04 14:29:06 +0800

    member information api

--- app/Http/Resources/MemberInformationResource.php
+++ app/Http/Resources/MemberInformationResource.php
@@ -18,7 +18,7 @@ class MemberInformationResource extends JsonResource
             'id' => $this->id,
             'user_id' => $this->user_id,
             'darbc_id' => $this->darbc_id,
-            'date_of_birth' => $this->date_of_birth,
+            'date_of_birth' => $this->date_of_birth?->format('Y-m-d'),
             'contact_number' => $this->contact_number,
             'user' => UserResource::make($this->whenLoaded('user')),
         ];
