ee7bade9c93f58b4402405eccbf37eab0a7130fc
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-03-26 09:29:03 +0800

    wip

--- resources/views/tables/columns/rsbsa/rsbsa-missing-details.blade.php
+++ resources/views/tables/columns/rsbsa/rsbsa-missing-details.blade.php
@@ -2,9 +2,9 @@
     @if($getRecord()->hasRsbsaRecord())
 
     @if($getRecord()->rsbsa->missing_details_count> 0)
-    <span class="inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset">{{$getRecord()->rsbsa->missing_details_count}}</span>
+    <span class="inline-flex items-center rounded-full bg-red-50   px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-gray-500/10 ring-inset">{{$getRecord()->rsbsa->missing_details_count}}</span>
     @else
-    <span class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">{{$getRecord()->rsbsa->missing_details_count}}</span>
+    <span class="inline-flex items-center  rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">{{$getRecord()->rsbsa->missing_details_count}}</span>
 
     @endif
 
